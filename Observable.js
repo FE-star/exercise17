936643c4c5c28c581fb1551f663f76d2e4e81fcf
侯先诚 <imhxc@houxianchengdeMacBook-Pro.local>
2018-02-07 14:32:57 +0800

    #

--- Observable.js
+++ Observable.js
@@ -7,16 +7,24 @@
 
 class ObserverList {
   constructor() {
-    this.observerList = [];
+    this.observerList = []; // observer列表
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    this.observerList.forEach((ele, index) => {
+      if (ele == observer) {
+        this.observerList.splice(index, 1)
+      }
+    })
+
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +34,18 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    let count = this.observers.count();
+    for(let i =0; i< count; i++) {
+      this.observers.observerList[i].update(...args)
+    }
   }
 }
 

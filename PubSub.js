3d6469b6a86c92b7d1203446a5d52ac4c44a8d80
王建忠 <yepbug@qq.com>
2018-02-07 14:58:03 +0800

    pass tests

--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,34 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    (this.subscribers[type]=this.subscribers[type] || []).push(fn);
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+   if(!fn) {
+     return this.subscribers[type] = [];
+   }
+   let i = this.subscribers[type].length - 1;
+   while(~i) {
+    if(fn === this.subscribers[type][i]) {
+      this.subscribers[type].splice(i, 1);
+      break;
+    }
+    i--;
+   }
   }
 
   publish(type, ...args) {
     // todo publish
+    if(!this.subscribers[type] || !this.subscribers[type].length) {
+      return;
+    }
+    let i = this.subscribers[type].length - 1;
+    while(~i) {
+      this.subscribers[type][i](...args);
+      i--;
+    }
   }
 
 }

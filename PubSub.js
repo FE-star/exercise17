22af802de69cb074252ec5a4c2fd00327199fe08
dingxiaofei <dingxiaofei@didichuxing.com>
2018-02-08 23:42:29 +0800

    作业17提交

--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,19 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    this.subscribers[type] = fn.bind(this);
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    if(!this.subscribers[type]) return false;
+    delete this.subscribers[type];
   }
 
   publish(type, ...args) {
     // todo publish
+    if(!this.subscribers[type]) return false;
+    this.subscribers[type].apply(this, args)
   }
 
 }

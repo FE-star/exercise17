6a122c492b15a1b07324af3ebdbd9db51a4eb011
favers <favers0601@gmail.com>
2018-02-08 16:43:00 +0800

    add

--- PubSub.js
+++ PubSub.js
@@ -6,21 +6,37 @@
  */
 
 module.exports = class PubSub {
-
   constructor() {
-    this.subscribers = {};
+    this.subscribers = {}
   }
 
   subscribe(type, fn) {
-    // todo subscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = []
+    }
+    this.subscribers[type].push(fn)
   }
 
   unsubscribe(type, fn) {
-    // todo unsubscribe
+    let list = this.subscribers[type]
+    if (!list) return false
+    if (!fn) {
+      list && (list.length = 0)
+    } else {
+      for (let i = list.length - 1; i >= 0; i--) {
+        let _fn = list[i]
+        if (_fn === fn) {
+          list.splice(i, 1)
+        }
+      }
+    }
   }
 
   publish(type, ...args) {
-    // todo publish
+    let list = this.subscribers[type]
+    if (!list || list.length === 0) return false
+    for (let i = 0, fn; (fn = list[i++]); ) {
+      fn.apply(this, args)
+    }
   }
-
 }

86420ccd7b9cf6e6a90f071c57981d97c08a0b95
Ivan <wcy5211678@163.com>
2018-02-09 19:56:36 +0800

    练习 17

--- PubSub.js
+++ PubSub.js
@@ -1,10 +1,10 @@
 /*
- * @Author: kael 
- * @Date: 2018-02-01 17:41:25 
+ * @Author: kael
+ * @Date: 2018-02-01 17:41:25
  * @Last Modified by: kael
  * @Last Modified time: 2018-02-02 17:39:45
  */
-
+//  发布订阅模式
 module.exports = class PubSub {
 
   constructor() {
@@ -13,14 +13,31 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (!this.subscribers[type])
+      this.subscribers[type] = []
+
+    this.subscribers[type].push(fn)
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    let subscribers = this.subscribers[type]
+    if (!subscribers) return
+
+    subscribers.forEach((val, index) => {
+      if(fn == val)
+        subscribers.splice(index, 1)
+    })
   }
 
   publish(type, ...args) {
     // todo publish
+    // console.log('publish', args);
+    if (!this.subscribers[type]) return
+
+    this.subscribers[type].forEach((fn) => {
+        fn(args[0])
+    })
   }
 
 }

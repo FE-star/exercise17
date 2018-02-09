b9a21c30508e8efa24cabb7aa3f9495a2b828f0b
suozhao <296954131@qq.com>
2018-02-09 11:47:02 +0800

    exercise17

--- Observable.js
+++ Observable.js
@@ -11,12 +11,15 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    return this.observerList.push( observer );
   }
   remove(observer) {
     // todo remove observer from list
+    this.observerList.splice( observer , 1 );
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +29,24 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add( observer );
   }
   removeObserver(observer) {
     // todo remove observer
+    let l = this.observers.count();
+    for (var i = l - 1; i >= 0; i--) {
+      if(this.observers.observerList[i] === observer){
+        this.observers.remove( i );
+      }
+    }
+    
   }
-  notify(...args) {
+  notify(context) {
     // todo notify
+    var observerCount = this.observers.count();
+    for(var i=0; i < observerCount; i++){
+      this.observers.observerList[i].update( context );
+    }
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,35 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if(!this.subscribers[type]){
+      this.subscribers[type] = [];
+    }
+    this.subscribers[type].push(fn)
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    let fns = this.subscribers[type];
+
+    if(!fn){
+      fns && (fns.length = 0);
+    }else{
+      for(let l = fns.length - 1; l >= 0; l--){
+        let _fn = fns[l];
+        if(_fn === fn){
+          fns.splice(l, 1);
+        }
+      }
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    let fns = this.subscribers[type]
+
+    for(let i = 0, fn; fn = fns[i++] ; ){
+      fn.call(this, ...args)
+    }
   }
 
 }

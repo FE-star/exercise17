4b892d9dfcf8fe4444a1b66cb1c5efd52d171946
dingxiaofei <dingxiaofei@didichuxing.com>
2018-02-10 14:14:47 +0800

    update pubsub

--- PubSub.js
+++ PubSub.js
@@ -2,7 +2,7 @@
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
  * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Last Modified time: 2018-02-02 17:39:45*/
  */
 
 module.exports = class PubSub {

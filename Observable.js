ad8f298cd3cb950254e4c4f5ff49fe7cb9159ae3
hookehuyr <hookehuyr@gmail.com>
2018-02-10 11:40:45 +0800

    finish

--- Observable.js
+++ Observable.js
@@ -1,6 +1,6 @@
 /*
- * @Author: kael 
- * @Date: 2018-02-01 17:41:25 
+ * @Author: kael
+ * @Date: 2018-02-01 17:41:25
  * @Last Modified by: kael
  * @Last Modified time: 2018-02-02 17:38:36
  */
@@ -10,13 +10,21 @@ class ObserverList {
     this.observerList = [];
   }
   add(observer) {
-    // todo add observer to list
+    var index = this.observerList.indexOf(observer)
+    if (index === -1) {
+      this.observerList.push(observer)
+    }
   }
   remove(observer) {
-    // todo remove observer from list
+    var index = this.observerList.indexOf(observer)
+    if (index >= 0) {
+      this.observerList.splice(index, 1)
+    } else {
+      return false
+    }
   }
   count() {
-    // return observer list size
+    return this.observerList.length
   }
 }
 
@@ -25,13 +33,16 @@ class Subject {
     this.observers = new ObserverList();
   }
   addObserver(observer) {
-    // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
-    // todo remove observer
+    this.observers.remove(observer)
   }
   notify(...args) {
-    // todo notify
+    var list = this.observers.observerList
+    for (var i = 0; i < list.length; i++) {
+      list[i].update(...args)
+    }
   }
 }
 

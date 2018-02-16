f8ed211c1e6e08d4244cd5b570eb22ddcbe12122
Summer <minya.su01@gmail.com>
2018-02-17 00:14:36 +0800

    练习

--- Observable.js
+++ Observable.js
@@ -1,6 +1,6 @@
 /*
- * @Author: kael 
- * @Date: 2018-02-01 17:41:25 
+ * @Author: kael
+ * @Date: 2018-02-01 17:41:25
  * @Last Modified by: kael
  * @Last Modified time: 2018-02-02 17:38:36
  */
@@ -10,13 +10,20 @@ class ObserverList {
     this.observerList = [];
   }
   add(observer) {
-    // todo add observer to list
+    return this.observerList.push(observer)
+  }
+  get(index) {
+      if(index > -1 && index < this.observerList.length){
+          return this.observerList[index]
+      }
   }
   remove(observer) {
-    // todo remove observer from list
+      this.observerList = this.observerList.filter((item) => {
+          return item !== observer
+      })
   }
   count() {
-    // return observer list size
+    return this.observerList.length
   }
 }
 
@@ -26,12 +33,18 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer)
   }
   notify(...args) {
     // todo notify
+    var count = this.observers.count()
+    for (var i = 0; i < count; i++) {
+        this.observers.get(i).update(...args)
+    }
   }
 }
 

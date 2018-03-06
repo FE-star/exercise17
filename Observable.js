b4309a5ee3ac8e9e243f288541aba57a33f29b86
qianqc <qianqc@minstone.com.cn>
2018-03-06 19:01:12 +0800

    finished

--- Observable.js
+++ Observable.js
@@ -1,38 +1,51 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:38:36
+ * @Last Modified by: Nico
+ * @Last Modified time: 2018-03-06 18:58:38
  */
 
 class ObserverList {
-  constructor() {
-    this.observerList = [];
-  }
-  add(observer) {
-    // todo add observer to list
-  }
-  remove(observer) {
-    // todo remove observer from list
-  }
-  count() {
-    // return observer list size
-  }
+    constructor() {
+        this.observerList = [];
+    }
+    add(observer) {
+        // todo add observer to list
+        this.observerList.push(observer)
+    }
+    remove(observer) {
+        // todo remove observer from list
+        let _this = this;
+        _this.observerList.forEach(function(val, key) {
+            if (observer === val) {
+                _this.observerList.splice(key, 1);
+            }
+        })
+    }
+    count() {
+        // return observer list size
+        return this.observerList.length;
+    }
 }
 
 class Subject {
-  constructor() {
-    this.observers = new ObserverList();
-  }
-  addObserver(observer) {
-    // todo add observer
-  }
-  removeObserver(observer) {
-    // todo remove observer
-  }
-  notify(...args) {
-    // todo notify
-  }
+    constructor() {
+        this.observers = new ObserverList();
+    }
+    addObserver(observer) {
+        // todo add observer
+        this.observers.add(observer);
+    }
+    removeObserver(observer) {
+        // todo remove observer
+        this.observers.remove(observer);
+    }
+    notify(...args) {
+        // todo notify
+        this.observers.observerList.forEach(function(val, key) {
+            val.update(...args);
+        })
+    }
 }
 
 module.exports = { Subject };
--- PubSub.js
+++ PubSub.js
@@ -1,26 +1,49 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Last Modified by: Nico
+ * @Last Modified time: 2018-03-06 18:58:38
  */
 
 module.exports = class PubSub {
 
-  constructor() {
-    this.subscribers = {};
-  }
+    constructor() {
+        this.subscribers = {};
+    }
 
-  subscribe(type, fn) {
-    // todo subscribe
-  }
+    subscribe(type, fn) {
+        // todo subscribe
+        if (!this.subscribers[type]) {
+            this.subscribers[type] = [];
+        }
+        this.subscribers[type].push({ fn });
+    }
 
-  unsubscribe(type, fn) {
-    // todo unsubscribe
-  }
+    unsubscribe(type, fn) {
+        // todo unsubscribe
+        if (!this.subscribers[type]) {
+            return false;
+        }
 
-  publish(type, ...args) {
-    // todo publish
-  }
+        let subscribers = this.subscribers[type];
+
+        subscribers.forEach((val, key) => {
+            if (val.fn === fn) {
+                subscribers.splice(key, 1);
+            }
+        });
+
+    }
+
+    publish(type, ...args) {
+        // todo publish
+        if (!this.subscribers[type]) {
+            return false;
+        }
+        let subscribers = this.subscribers[type];
+        for (let i = 0; i < subscribers.length; i++) {
+            subscribers[i]['fn'](...args);
+        }
+    }
 
 }

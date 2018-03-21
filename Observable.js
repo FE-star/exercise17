3245ab37dff6f50470faf150088f62b326b115e2
shen <357789504@qq.com>
2018-03-21 22:44:00 +0800

    add(Observer):完成观察者填空

--- Observable.js
+++ Observable.js
@@ -5,33 +5,45 @@
  * @Last Modified time: 2018-02-02 17:38:36
  */
 
+ //观察者列表
 class ObserverList {
   constructor() {
     this.observerList = [];
   }
   add(observer) {
-    // todo add observer to list
+    // todo add observer to list,return length
+    return this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    this.observerList.splice(this.observerList.indexOf(observer),1);
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
+//目标
 class Subject {
   constructor() {
     this.observers = new ObserverList();
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    let observerCount = this.observers.count();
+    for(let mm in this.observers.observerList){
+      this.observers.observerList[mm].update(...args);
+    }
+
   }
 }
 

011565e8cb368515eda7da73202dfbaf37643e10
ZhangLynn <1583454131@qq.com>
2018-06-09 22:00:24 +0800

    完成观察者模式和发布/订阅模式的代码

--- Observable.js
+++ Observable.js
@@ -1,38 +1,49 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:38:36
+ * @Last Modified by: LynnZhang
+ * @Last Modified time: 2018-06-09 22:00:00
  */
 
 class ObserverList {
-  constructor() {
-    this.observerList = [];
-  }
-  add(observer) {
-    // todo add observer to list
-  }
-  remove(observer) {
-    // todo remove observer from list
-  }
-  count() {
-    // return observer list size
-  }
+    constructor() {
+        this.observerList = [];
+    }
+
+    add(observer) {
+        this.observerList.push(observer)
+    }
+
+    remove(observer) {
+        let hasIndex = this.observerList.indexOf(observer);
+        if (hasIndex > -1) {
+            this.observerList.splice(hasIndex, 1)
+        }
+    }
+
+    count() {
+        return this.observerList.length
+    }
 }
 
 class Subject {
-  constructor() {
-    this.observers = new ObserverList();
-  }
-  addObserver(observer) {
-    // todo add observer
-  }
-  removeObserver(observer) {
-    // todo remove observer
-  }
-  notify(...args) {
-    // todo notify
-  }
+    constructor() {
+        this.observers = new ObserverList();
+    }
+
+    addObserver(observer) {
+        this.observers.add(observer)
+    }
+
+    removeObserver(observer) {
+        this.observers.remove(observer)
+    }
+
+    notify(...args) {
+        this.observers.observerList.map(observer => {
+            observer.update(...args)
+        })
+    }
 }
 
-module.exports = { Subject };
+module.exports = {Subject};
--- PubSub.js
+++ PubSub.js
@@ -1,26 +1,30 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Last Modified by: LynnZhang
+ * @Last Modified time: 2018-06-09 22:00:00
  */
 
 module.exports = class PubSub {
 
-  constructor() {
-    this.subscribers = {};
-  }
+    constructor() {
+        this.subscribers = {};
+    }
 
-  subscribe(type, fn) {
-    // todo subscribe
-  }
+    subscribe(type, fn) {
+        this.subscribers[type]=fn
+    }
 
-  unsubscribe(type, fn) {
-    // todo unsubscribe
-  }
+    unsubscribe(type, fn) {
+        delete this.subscribers[type]
+    }
 
-  publish(type, ...args) {
-    // todo publish
-  }
+    publish(type, ...args) {
+        if(!this.subscribers[type]){
+            return
+        }
+        let fn=this.subscribers[type];
+        fn(...args)
+    }
 
 }

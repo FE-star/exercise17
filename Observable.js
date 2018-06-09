543d6bc4d9a72f18c20d2b35d27e291bc350c566
Chenjiayuan195 <30591313+Chenjiayuan195@users.noreply.github.com>
2018-06-09 17:11:50 +0800

    提交作业

--- Observable.js
+++ Observable.js
@@ -10,12 +10,17 @@ class ObserverList {
     this.observerList = [];
   }
   add(observer) {
+    this.observerList.push(observer);
     // todo add observer to list
   }
   remove(observer) {
+    this.observerList=this.observerList.filter(items=>{
+      return items!=observer;
+    })
     // todo remove observer from list
   }
   count() {
+    return this.observerList.length;
     // return observer list size
   }
 }
@@ -25,12 +30,17 @@ class Subject {
     this.observers = new ObserverList();
   }
   addObserver(observer) {
+    this.observers.add(observer);
     // todo add observer
   }
   removeObserver(observer) {
+    this.observers.remove(observer);
     // todo remove observer
   }
   notify(...args) {
+    this.observers.observerList.forEach(items => {
+      items.update(...args);
+    });
     // todo notify
   }
 }
--- PubSub.js
+++ PubSub.js
@@ -12,14 +12,31 @@ module.exports = class PubSub {
   }
 
   subscribe(type, fn) {
+    if (!this.subscribers[type]){
+      this.subscribers[type]=[];
+    }
+    this.subscribers[type].push(fn);
     // todo subscribe
   }
 
   unsubscribe(type, fn) {
+    if (!this.subscribers[type]){
+      return;
+    }
+    this.subscribers[type] = this.subscribers[type].filter(items=>{
+      items!=fn;
+    })
+    
     // todo unsubscribe
   }
 
   publish(type, ...args) {
+    if (!this.subscribers[type]) {
+      return;
+    }
+    this.subscribers[type].forEach(items => {
+      items.apply(this,args);
+    });
     // todo publish
   }
 

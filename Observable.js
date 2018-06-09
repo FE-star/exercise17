f82216f3f73569789ecf510cadeeda840c3a0f0c
zhuanyongxigua <zhuanyongxigua@gmail.com>
2018-06-09 14:38:47 +0800

    finish the exercise

--- Observable.js
+++ Observable.js
@@ -11,12 +11,15 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    return this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    this.observerList.splice(this.observerList.indexOf(observer), 1);
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +29,18 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    var observerCount = this.observers.count();
+    for(var i=0; i < observerCount; i++){
+      this.observers.observerList[i].update(...args);
+    }
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,35 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (!this.subscribers[type]) this.subscribers[type] = [];
+    this.subscribers[type].push({func: fn});
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    for (var m in this.subscribers) {
+      if (m === type) {
+        for (var i = 0, j = this.subscribers[m].length; i < j; i++) {
+          if (this.subscribers[m][i].func === fn) {
+            this.subscribers[m].splice( i, 1 );
+          }
+        }
+      }
+    }
+    return this;
   }
 
   publish(type, ...args) {
     // todo publish
+    if (!this.subscribers[type]) {
+      return false;
+    }
+    var subscribers = this.subscribers[type],
+    len = subscribers ? subscribers.length : 0;
+    while (len--) {
+      subscribers[len].func(...args);
+    }
+    return this;
   }
 
 }

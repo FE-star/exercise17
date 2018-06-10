3299232283f93a9eb3b5a7ebd390f1967e334355
xiaobai <bingxue0612@qq.com>
2018-06-10 13:31:28 +0800

    刘俊峰 exercise17

--- PubSub.js
+++ PubSub.js
@@ -1,8 +1,8 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-06-10 13:11:16
  */
 
 module.exports = class PubSub {
@@ -12,15 +12,37 @@ module.exports = class PubSub {
   }
 
   subscribe(type, fn) {
-    // todo subscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = []
+    }
+    this.subscribers[type].push(fn)
   }
 
   unsubscribe(type, fn) {
-    // todo unsubscribe
+    let fns = this.subscribers[type]
+    if (!fns) {
+      return false
+    }
+    if (!fn) {
+      fns && (fns.length = 0)
+    } else {
+      for (let i = fns.length - 1; i >= 0; i--) {
+        let _fn = fns[i]
+        if (_fn === fn) {
+          fns.splice(i, 1)
+        }
+      }
+    }
   }
 
   publish(type, ...args) {
-    // todo publish
+    let fns = this.subscribers[type]
+    if (!fns || fns.length === 0) {
+      return false
+    }
+    for (let i =0; i < fns.length; i++) {
+      fns[i](...args)
+    }
   }
 
 }

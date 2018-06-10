fdeb7094207ab727d0f402f7843accb192b81d70
guoekzhibu <850997448@qq.com>
2018-06-10 18:46:00 +0800

    exercise17 test

--- Observable.js
+++ Observable.js
@@ -11,13 +11,17 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    this.observerList.splice(this.observerList.findIndex(item => item === observer), 1);
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
+
 }
 
 class Subject {
@@ -26,12 +30,17 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    for(let item of this.observers.observerList){
+      item.update( ...args );
+    }
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,31 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if(!this.subscribers[type]){
+        this.subscribers[type] = [];
+    }
+    this.subscribers[type].push(fn);
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    for(let key in this.subscribers){
+      if(key == type){
+        for ( let i=0,len = this.subscribers[key].length;i<len;i++ ) {
+          if ( this.subscribers[key][i] == fn ) {
+              this.subscribers[key].splice( i, 1 );
+              return;
+          }
+        }
+      }
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    for(let item of this.subscribers[type]){
+        item(...args);
+    }
   }
 
 }

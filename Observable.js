1f304110ea3883eca813713a87ca3d9f58a0f26d
Little Rui <1043448355@qq.com>
2018-06-17 19:40:05 +0800

    flcwl's answer to exercise-17

--- Observable.js
+++ Observable.js
@@ -1,22 +1,41 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:38:36
+ * @Last Modified by: flcwl
+ * @Last Modified time: 2018-06-17 14:29:49
  */
 
 class ObserverList {
   constructor() {
     this.observerList = [];
   }
+
   add(observer) {
     // todo add observer to list
+    if(this.observerList.includes(observer)) {
+      return true; // 可以更新observer
+    }
+    return this.observerList.push(observer);
   }
+
   remove(observer) {
     // todo remove observer from list
+    let index = -1;
+    for (let i = 0; i < this.count(); i++) {
+      if (this.observerList[i] == observer) { // 可二分 抽离出一个get方法
+        index = i;
+      }
+    }
+    if (index === -1) {
+      return true;
+    }
+    this.observerList[index] = null;
+    this.observerList.splice(index, 1);
   }
+
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -24,15 +43,26 @@ class Subject {
   constructor() {
     this.observers = new ObserverList();
   }
+
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
+  
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
+
   notify(...args) {
     // todo notify
+    this.observers.observerList.forEach((observer) => {
+      if (observer.update) {
+        observer.update(...args);
+      }
+    });
   }
+
 }
 
 module.exports = { Subject };
--- PubSub.js
+++ PubSub.js
@@ -1,8 +1,8 @@
 /*
  * @Author: kael 
  * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Last Modified by: flcwl
+ * @Last Modified time: 2018-06-17 13:36:05
  */
 
 module.exports = class PubSub {
@@ -13,14 +13,31 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    switch (type) {
+      case 'add': 
+        this.subscribers.add = fn;
+        break;
+    }
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    switch (type) {
+      case 'add': 
+        this.subscribers.add = null;
+        break;
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    switch (type) {
+      case 'add': 
+        if (this.subscribers.add) {
+          return this.subscribers.add(...args);
+        }
+        break;
+    }
   }
 
 }

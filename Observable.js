b69cdf0376a6645acdc90b749fd9b0c3f7711cd7
zhangruijie <15910256180@163.com>
2018-11-13 00:41:54 +0800

    complete Observable

--- Observable.js
+++ Observable.js
@@ -11,12 +11,25 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer);
+  }
+  get(index) {
+    if(index > -1 && index < this.observerList.length){
+      return this.observerList[index];
+    }
   }
   remove(observer) {
     // todo remove observer from list
+    var observerListLen = this.observerList.length;
+    for(var i = 0;i < observerListLen;i++){
+      if(this.observerList[i] === observer){
+        this.observerList.splice(i,1);
+      }
+    }
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +39,20 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    var obseversCount = this.observers.count();
+    for(var i = 0; i< obseversCount;i++){
+      for(var arg of args) {
+        this.observers.get(i).update(arg);
+      }
+    }
   }
 }
 

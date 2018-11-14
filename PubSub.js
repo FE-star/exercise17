5e14a28a923a6527dfcc2b9a00b66065894c450c
朴野归踪 <nemoro1928@163.com>
2018-11-14 23:18:07 +0800

    这是我做的最快的一次作业了
    add

--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,40 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (this.subscribers[type] === undefined) {
+      this.subscribers[type] = [fn];
+    } else {
+      this.subscribers[type].push(fn);
+    }
+
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    if (this.subscribers[type] === undefined) {
+      return false;
+    } else {
+      let queue = this.subscribers[type];
+      let index = 0;
+      queue.forEach(element => {
+        if(element === fn){
+          queue.splice(index, 1)
+        }
+        index++;
+      });
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    if (this.subscribers[type] === undefined) {
+      return false;
+    } else {
+      let queue = this.subscribers[type];
+      queue.forEach(element => {
+        element.apply(this, args)
+      });
+    }
   }
 
 }

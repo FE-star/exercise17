7ebec89fe832a5cbb844ff11dad4b18b1c2bc504
chenyu <iambigchen@163.com>
2018-11-15 13:52:08 +0800

    预习17

--- Observable.js
+++ Observable.js
@@ -10,12 +10,20 @@ class ObserverList {
     this.observerList = [];
   }
   add(observer) {
+    this.observerList.push(observer)
     // todo add observer to list
   }
   remove(observer) {
+    for (let index = 0; index < this.observerList.length; index++) {
+      const element = this.observerList[index];
+      if (element == observer) {
+        this.observerList.splice(index, 1)
+      }
+    }
     // todo remove observer from list
   }
   count() {
+    return this.observerList.length
     // return observer list size
   }
 }
@@ -26,12 +34,18 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    console.log(observer);
+    this.observers.add(observer)
   }
   removeObserver(observer) {
+    this.observers.remove(observer)
     // todo remove observer
   }
   notify(...args) {
     // todo notify
+    this.observers.observerList.forEach(e => {
+      e.update.call(e, ...args)
+    })
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,13 +13,46 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    this.subscribers[type] = this.subscribers[type] || []
+    this.subscribers[type]['callback'] = this.subscribers[type]['callback'] || []
+    this.subscribers[type]['data'] = this.subscribers[type]['data'] || []
+    if (fn) {
+      this.subscribers[type]['callback'].push(fn)
+    }
+    for (let index = 0; index < this.subscribers[type]['callback'].length; index++) {
+      for (let j = 0; j < this.subscribers[type]['data'].length; j++) {
+        const element = array[j];
+        this.subscribers[type]['callback'][index](element)
+      }
+    }  
+    this.subscribers[type]['data'] = []
   }
 
   unsubscribe(type, fn) {
+    this.subscribers[type] = this.subscribers[type] || []
+    this.subscribers[type]['callback'] = this.subscribers[type]['callback'] || []
+    this.subscribers[type]['data'] = this.subscribers[type]['data'] || []
+    for (let index = 0; index < this.subscribers[type]['callback'].length; index++) {
+      if (this.subscribers[type]['callback'][index] == fn) {
+        this.subscribers[type]['callback'].splice(index, 1)
+      }
+    }  
     // todo unsubscribe
   }
 
   publish(type, ...args) {
+    this.subscribers[type] = this.subscribers[type] || []
+    this.subscribers[type]['data'] = this.subscribers[type]['data'] || []
+    args.forEach(e => {
+      this.subscribers[type]['data'].push(e)
+    })
+    for (let index = 0; index < this.subscribers[type]['callback'].length; index++) {
+      for (let j = 0; j < this.subscribers[type]['data'].length; j++) {
+        const element = this.subscribers[type]['data'][j];
+        this.subscribers[type]['callback'][index](element)
+      }
+    }
+    this.subscribers[type]['data'] = []
     // todo publish
   }
 

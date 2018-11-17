ea96238157e14a5c71647d79f9f2af2138848166
hutn <32947142+hutn@users.noreply.github.com>
2018-11-17 21:56:43 +0800

    Update Observable.js

--- Observable.js
+++ Observable.js
@@ -11,12 +11,21 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    let index = 0;
+    this.observerList.forEach(element => {
+      if(element === observer){
+        this.observerList.splice(index, 1)
+      }
+      index++;
+    });
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +35,17 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove( this.observers.observerList.indexOf(observer) );
   }
   notify(...args) {
     // todo notify
+    this.observers.observerList.map( observer => {
+        observer.update.call( observer, ...args);
+    })
   }
 }
 

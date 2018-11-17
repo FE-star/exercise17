25da3d551a041388349ebef80685ec9bba04cfc5
何智 <1321120469@qq.com>
2018-11-17 22:49:36 +0800

    观察者模式与发布/订阅模式-炼心

--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,23 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = [fn];
+    } else {
+      this.subscribers[type] = this.subscribers[type].concat(fn);
+    }
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    if (!this.subscribers[type]) return;
+    this.subscribers[type] = this.subscribers[type].filter(func => func != fn);
   }
 
   publish(type, ...args) {
     // todo publish
+    if (!this.subscribers[type]) return;
+    this.subscribers[type].map(func => func.call(this, ...args));
   }
 
 }

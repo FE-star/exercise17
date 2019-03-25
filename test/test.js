43c2d256f509eace7da66860b5b2af66ee6d3372
邓聪 <469376974@qq.com>
2019-03-25 16:45:57 +0800

    finish commit

--- test/test.js
+++ test/test.js
@@ -6,70 +6,71 @@
  */
 
 const assert = require('assert');
-const { PubSub, Observable } = require('..');
+const {PubSub, Observable} = require('..');
 
 const random = () => Math.random() * 1e9 >> 0 || 1;
 
 describe('PubSub', () => {
-  let ob = new PubSub();
+    let ob = new PubSub();
 
-  it('subscribe -> publish', async () => {
-    let sum = 0;
-    let val = random();
-    ob.subscribe('add', (val) => sum += val);
-    ob.publish('add', val);
-    assert.ok(sum === val);
-  });
+    it('subscribe -> publish', async () => {
+        let sum = 0;
+        let val = random();
+        ob.subscribe('add', (val) => sum += val);
+        ob.publish('add', val);
+        assert.ok(sum === val);
+    });
 
-  it('subscribe -> publish -> unsubscribe -> publish', async () => {
-    let sum = 0;
-    let val = random();
-    let add = (val) => sum += val;
-    ob.subscribe('add', add);
-    ob.publish('add', val);
-    assert.ok(sum === val);
+    it('subscribe -> publish -> unsubscribe -> publish', async () => {
+        let sum = 0;
+        let val = random();
+        let add = (val) => sum += val;
+        ob.subscribe('add', add);
+        ob.publish('add', val);
+        assert.ok(sum === val);
 
-    sum = 0;
-    val = random();
-    ob.unsubscribe('add', add);
-    ob.publish('add', val);
-    assert.ok(sum !== val);
-  });
+        sum = 0;
+        val = random();
+        ob.unsubscribe('add', add);
+        ob.publish('add', val);
+        assert.ok(sum !== val);
+    });
 });
 
 describe('Observable', () => {
-  class Observer {
-    constructor() {
-      this.sum = 0;
+    class Observer {
+        constructor() {
+            this.sum = 0;
+        }
+
+        update(val) {
+            this.sum += val;
+        }
     }
-    update(val) {
-      this.sum += val;
-    }
-  }
 
-  it('addObserver -> notify', async () => {
-    let subject = new Observable.Subject();
-    let ob = new Observer();
-    subject.addObserver(ob);
-    assert.ok(subject.observers.count() === 1);
-    let val = random();
-    subject.notify(val);
-    assert.ok(ob.sum === val);
-  });
+    it('addObserver -> notify', async () => {
+        let subject = new Observable.Subject();
+        let ob = new Observer();
+        subject.addObserver(ob);
+        assert.ok(subject.observers.count() === 1);
+        let val = random();
+        subject.notify(val);
+        assert.ok(ob.sum === val);
+    });
 
-  it('addObserver -> notify -> removeObserver -> notify', async () => {
-    let subject = new Observable.Subject();
-    let ob = new Observer();
-    subject.addObserver(ob);
-    assert.ok(subject.observers.count() === 1);
-    let val = random();
-    subject.notify(val);
+    it('addObserver -> notify -> removeObserver -> notify', async () => {
+        let subject = new Observable.Subject();
+        let ob = new Observer();
+        subject.addObserver(ob);
+        assert.ok(subject.observers.count() === 1);
+        let val = random();
+        subject.notify(val);
 
-    ob.sum = 0;
-    val = random();
-    subject.removeObserver(ob);
-    assert.ok(subject.observers.count() === 0);
-    subject.notify(val);
-    assert.ok(ob.sum !== val);
-  });
+        ob.sum = 0;
+        val = random();
+        subject.removeObserver(ob);
+        assert.ok(subject.observers.count() === 0);
+        subject.notify(val);
+        assert.ok(ob.sum !== val);
+    });
 });

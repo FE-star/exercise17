cc293d9865bbe382399eb8ab24664b80bb42a381
zerrrrr <liyingze0115@qq.com>
2019-03-28 23:38:23 +0800

    李英泽-exercise17

--- Observable.js
+++ Observable.js
@@ -11,12 +11,23 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer);
   }
   remove(observer) {
     // todo remove observer from list
+    if(!observer){
+      return;
+    }
+    for(let i=0;i<this.observerList.length;i++){
+      let item = this.observerList[i]
+      if(item === observer){
+        this.observerList.splice(i, 1); //删除对于的订阅
+      }
+    }
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +37,20 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    if(this.observers.count() === 0){
+      return;
+    }
+    for(let i=0;i<this.observers.count();i++){
+      this.observers.observerList[i].update(...args);
+    }
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,39 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if(!this.subscribers[type]){
+      this.subscribers[type] = [];
+    }
+    this.subscribers[type].push(fn); 
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    let fns = this.subscribers[type]
+    if(!fns || !fns.length){
+      return;
+    }
+    if(!fn){
+      fns = []
+    }else{
+      for(let i=0;i<fns.length;i++){
+        let item = fns[i]
+        if(item === fn){
+          fns.splice(i, 1); //删除订阅
+        }
+      }
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    let fns = this.subscribers[type]
+    if(!fns || !fns.length){
+      return;
+    }
+    for(let i=0;i<fns.length;i++){
+      fns[i].apply(this, args);
+    }
   }
 
 }

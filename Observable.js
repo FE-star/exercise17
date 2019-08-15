dc5110bc1c7f8ebc3be48d07e2e94e614782b567
herott <294963786@qq.com>
2019-08-15 23:15:15 +0800

    提交练习

--- Observable.js
+++ Observable.js
@@ -11,12 +11,22 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer)
   }
   remove(observer) {
     // todo remove observer from list
+    let inx = this.observerList.indexOf(observer)
+    if (inx > -1) {
+      this.observerList.splice(inx, 1)
+      return true
+    } else {
+      return '没有该观察者'
+    }
+
   }
   count() {
     // return observer list size
+    return this.observerList.length
   }
 }
 
@@ -26,12 +36,17 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer)
   }
   notify(...args) {
     // todo notify
+    this.observers.observerList.forEach(observer => {
+      observer.update && observer.update(...args)
+    })
   }
 }
 
